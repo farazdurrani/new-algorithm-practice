83fa68a67a49caa305562e326a69f24e99720b28
Faraz Durrani <farazkhurram@msn.com>
2024-01-22 20:25:37 -0600

    mateix

--- src/main/java/matrix/MatrixMultiplicationNxM.java
+++ src/main/java/matrix/MatrixMultiplicationNxM.java
@@ -26,11 +26,9 @@ public class MatrixMultiplicationNxM {
     int[][] c = new int[aRows][bColumns];
     for (int i = 0; i < aRows; i++) {
       for (int j = 0; j < bColumns; j++) {
-        int sum = 0;
         for (int k = 0; k < aColumns; k++) {
-          sum = sum + (a[i][k] * b[k][j]);
+          c[i][j] += (a[i][k] * b[k][j]);
         }
-        c[i][j] = sum;
       }
     }
     //print
@@ -52,7 +50,10 @@ public class MatrixMultiplicationNxM {
     a[2][0] = 7;
     a[2][1] = 8;
     a[2][2] = 9;
-
+//    Elements of matrix a:
+//    1 2 3
+//    4 5 6
+//    7 8 9
     b[0][0] = 9;
     b[0][1] = 8;
     b[0][2] = 7;
@@ -62,5 +63,9 @@ public class MatrixMultiplicationNxM {
     b[2][0] = 3;
     b[2][1] = 2;
     b[2][2] = 1;
+//    Elements of matrix b
+//    9 8 7
+//    6 5 4
+//    3 2 1
   }
 }
